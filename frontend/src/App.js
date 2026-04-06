e8ee9c2a2bc22160416ae2c0f0391d0a069eba0d
Daniel Gililov <156680768+FunnyShaker@users.noreply.github.com>
2026-04-06 12:54:42 -0400

    Add cart and viewflights

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 import Login from "./pages/login"
 import Signup from "./pages/signUp"
-import Home from "./pages/home"
+import ViewFlights from "./pages/viewFlights"
+import Cart from "./pages/cart"
 
 function App() {
   return (
     <BrowserRouter>
+
       <Routes>
+
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/flights" element={<ViewFlights />} />
+        <Route path="/cart" element={<Cart />} />
+
       </Routes>
+
     </BrowserRouter>
   )
 }
